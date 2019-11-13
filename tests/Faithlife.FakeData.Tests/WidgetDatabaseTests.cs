91919a6224eb69b365661e4c972f633d2087271d
Ed Ball <ed.ball@faithlife.com>
2019-11-13 10:21:59 -0800

    Use using declarations.

--- tests/Faithlife.FakeData.Tests/WidgetDatabaseTests.cs
+++ tests/Faithlife.FakeData.Tests/WidgetDatabaseTests.cs
@@ -14,46 +14,42 @@ namespace Faithlife.FakeData.Tests
 		public void CrudTests()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
+			using var context = database.Lock();
+			var records = context.Widgets.AddRange(new[]
 			{
-				var records = context.Widgets.AddRange(new[]
-				{
-					new WidgetRecord { Name = "Alice" },
-					new WidgetRecord { Name = "Bob" },
-				});
+				new WidgetRecord { Name = "Alice" },
+				new WidgetRecord { Name = "Bob" },
+			});
 
-				var aliceId = records[0].WidgetId;
-				var bobId = records[1].WidgetId;
+			var aliceId = records[0].WidgetId;
+			var bobId = records[1].WidgetId;
 
-				context.Widgets.Single(x => x.WidgetId == aliceId).Name.Should().Be("Alice");
-				context.Widgets.Single(x => x.WidgetId == bobId).Name.Should().Be("Bob");
+			context.Widgets.Single(x => x.WidgetId == aliceId).Name.Should().Be("Alice");
+			context.Widgets.Single(x => x.WidgetId == bobId).Name.Should().Be("Bob");
 
-				context.Widgets.UpdateWhere(x => x.WidgetId == bobId, x => x.Name = "Robert").Should().Be(1);
-				context.Widgets.RemoveWhere(x => x.WidgetId == aliceId).Should().Be(1);
+			context.Widgets.UpdateWhere(x => x.WidgetId == bobId, x => x.Name = "Robert").Should().Be(1);
+			context.Widgets.RemoveWhere(x => x.WidgetId == aliceId).Should().Be(1);
 
-				context.Widgets.Any(x => x.WidgetId == aliceId).Should().BeFalse();
-				context.Widgets.Single(x => x.WidgetId == bobId).Name.Should().Be("Robert");
-			}
+			context.Widgets.Any(x => x.WidgetId == aliceId).Should().BeFalse();
+			context.Widgets.Single(x => x.WidgetId == bobId).Name.Should().Be("Robert");
 		}
 
 		[Test]
 		public async Task LockAsync()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = await database.LockAsync(CancellationToken.None).ConfigureAwait(false))
-				context.Widgets.Add(new WidgetRecord { Name = "Alice" });
+			using var context = await database.LockAsync(CancellationToken.None).ConfigureAwait(false);
+			context.Widgets.Add(new WidgetRecord { Name = "Alice" });
 		}
 
 		[Test]
 		public void NullRecord()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
-			{
-				Invoking(() => context.Widgets.Add(null!)).Should().Throw<ArgumentNullException>();
-				Invoking(() => context.Widgets.AddRange(null!)).Should().Throw<ArgumentNullException>();
-				Invoking(() => context.Widgets.AddRange(new WidgetRecord[] { null! })).Should().Throw<ArgumentNullException>();
-			}
+			using var context = database.Lock();
+			Invoking(() => context.Widgets.Add(null!)).Should().Throw<ArgumentNullException>();
+			Invoking(() => context.Widgets.AddRange(null!)).Should().Throw<ArgumentNullException>();
+			Invoking(() => context.Widgets.AddRange(new WidgetRecord[] { null! })).Should().Throw<ArgumentNullException>();
 		}
 
 		[Test]
@@ -73,45 +69,43 @@ namespace Faithlife.FakeData.Tests
 		public void SetNextAutoId()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
-			{
-				context.Widgets.SetNextAutoId(123);
-				context.Widgets.GetNextAutoId().Should().Be(123);
-				context.Widgets.Add(new WidgetRecord { Name = "Alice" }).WidgetId.Should().Be(123);
-				context.Widgets.GetNextAutoId().Should().Be(124);
-			}
+			using var context = database.Lock();
+			context.Widgets.SetNextAutoId(123);
+			context.Widgets.GetNextAutoId().Should().Be(123);
+			context.Widgets.Add(new WidgetRecord { Name = "Alice" }).WidgetId.Should().Be(123);
+			context.Widgets.GetNextAutoId().Should().Be(124);
 		}
 
 		[Test]
 		public void NameRequired()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
-				Invoking(() => context.Widgets.Add(new WidgetRecord { Name = null! })).Should().Throw<InvalidOperationException>();
+			using var context = database.Lock();
+			Invoking(() => context.Widgets.Add(new WidgetRecord { Name = null! })).Should().Throw<InvalidOperationException>();
 		}
 
 		[Test]
 		public void NameTooLong()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
-				Invoking(() => context.Widgets.Add(new WidgetRecord { Name = new string('X', 101) })).Should().Throw<InvalidOperationException>();
+			using var context = database.Lock();
+			Invoking(() => context.Widgets.Add(new WidgetRecord { Name = new string('X', 101) })).Should().Throw<InvalidOperationException>();
 		}
 
 		[Test]
 		public void ValidCreatedDate()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
-				context.Widgets.Add(new WidgetRecord { Name = "Alice", Created = "2000-01-02T03:04:05Z" });
+			using var context = database.Lock();
+			context.Widgets.Add(new WidgetRecord { Name = "Alice", Created = "2000-01-02T03:04:05Z" });
 		}
 
 		[Test]
 		public void InvalidCreatedDate()
 		{
 			var database = FakeDatabase.Create<WidgetDatabaseContext>();
-			using (var context = database.Lock())
-				Invoking(() => context.Widgets.Add(new WidgetRecord { Name = "Alice", Created = "2000-01-02" })).Should().Throw<InvalidOperationException>();
+			using var context = database.Lock();
+			Invoking(() => context.Widgets.Add(new WidgetRecord { Name = "Alice", Created = "2000-01-02" })).Should().Throw<InvalidOperationException>();
 		}
 
 		private static Action Invoking(Action action) => action;
