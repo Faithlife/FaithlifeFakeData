50369fb1feb86f4b9bdbe37e80e25b1af0136ac0
Ed Ball <ed.ball@faithlife.com>
2021-02-19 15:42:28 -0800

    Recombine FakeDatabase types into one file.

--- src/Faithlife.FakeData/FakeDatabase.cs
+++ src/Faithlife.FakeData/FakeDatabase.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,5 +17,44 @@ namespace Faithlife.FakeData
 		{
 			return new FakeDatabase<T>(new T());
 		}
+	}
+
+	/// <summary>
+	/// A fake database.
+	/// </summary>
+	[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Generic type.")]
+	public sealed class FakeDatabase<TContext>
+		where TContext : FakeDatabaseContext
+	{
+		/// <summary>
+		/// Locks the database.
+		/// </summary>
+		/// <remarks>Database tables and records should only be accessed while the database is locked.
+		/// Dispose the returned context to unlock the database.</remarks>
+		public TContext Lock()
+		{
+			var context = (TContext) m_context.ShallowClone();
+			context.Lock();
+			return context;
+		}
+
+		/// <summary>
+		/// Locks the database.
+		/// </summary>
+		/// <remarks>Database tables and records should only be accessed while the database is locked.
+		/// Dispose the returned context to unlock the database.</remarks>
+		public async Task<TContext> LockAsync(CancellationToken cancellationToken)
+		{
+			var context = (TContext) m_context.ShallowClone();
+			await context.LockAsync(cancellationToken).ConfigureAwait(false);
+			return context;
+		}
+
+		internal FakeDatabase(TContext context)
+		{
+			m_context = context;
+		}
+
+		private readonly TContext m_context;
 	}
 }
